65dedf4f6d760a3cf5a786a55412846b6af28225
Timur Uzel <timurt.uzel@gmail.com>
2016-09-04 17:55:23 -0400

    first

---
+++ main.js
@@ -0,0 +1,36 @@
+/**
+ * Created by timur on 9/4/16.
+ */
+
+import {app, BrowserWindow} from 'electron'
+
+let win
+
+function createWindow() {
+  
+  win = new BrowserWindow({width: 800, height: 600})
+  
+  win.loadURL(`file://${dirname}/index.html`)
+  
+  win.webContents.openDevTools()
+  
+  win.on('closed', () => {
+    win = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  
+  if(process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  
+  if(win === null) {
+    createWindow()
+  }
+})
